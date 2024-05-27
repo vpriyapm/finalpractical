0a88a58689269bc57779b66ec0431e34757e9a8c
akhil-alpha <akhilsaji2004@gmail.com>
2024-05-27 17:15:21 +0530

    changed

--- num.c
+++ num.c
@@ -3,19 +3,11 @@
 int main()
 {
     int num1, num2, sum;
-<<<<<<< HEAD
 
-    printf("30: ");
-    scanf("%d", &num1);
-
-    printf("Enter the second number: ");
-=======
-    
     printf("10: ");
     scanf("%d", &num1);
-    
+
     printf("20: ");
->>>>>>> 28d1c4c44d607f8932f9d51cdaf15b90174cbaa7
     scanf("%d", &num2);
 
     sum = num1 + num3;
